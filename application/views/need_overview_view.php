a3054f1ecbdbe6fc112854d69a22809ac6a34188
Victor Alberto Aguilar Hernandez <Victor Alberto Aguilar Hernandez>
2017-06-12 02:21:24 -0600

    sfd

---
+++ application/views/need_overview_view.php
@@ -0,0 +1,36 @@
+<section class="need_overview">
+    <div class="container">
+        <div class="col-xs-6">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatem quam nam unde obcaecati voluptatum officia veniam quia similique placeat magnam quo, molestiae doloremque recusandae libero adipisci ipsum rem deleniti veritatis maiores, inventore dicta, ab officiis. Voluptates, perspiciatis, ab. Debitis molestiae corporis cupiditate, esse eum, aspernatur ea sequi cum ipsum deleniti a, pariatur cumque hic maxime iure libero doloribus, quas enim commodi. Ipsam iusto, fugit aperiam, saepe alias accusantium adipisci, pariatur molestias quis recusandae voluptatem iste tempora aut laboriosam doloremque explicabo amet veniam at enim dolorum labore! Animi minus, corporis laborum perferendis, ipsum quasi ullam ducimus voluptatibus voluptate, culpa quaerat non.</div>
+        <div class="col-xs-6">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nobis ad, accusamus perferendis cupiditate ratione enim temporibus vitae atque similique, maiores aspernatur illum ipsum. Sunt assumenda quia molestias soluta illum quaerat ullam? Architecto veniam reprehenderit dolores, eos nesciunt rerum, soluta, qui sequi consectetur dolorum quod unde similique ipsa distinctio vel iure. Vero tempore vel ullam iure maiores, temporibus, animi, consequuntur in possimus eveniet cumque itaque soluta fugiat laborum voluptate praesentium repellendus. Et error ad voluptate excepturi distinctio, cum quo iusto eos qui. Ut dolores, consectetur neque nam, consequuntur nihil? Ut laboriosam neque, animi officiis nisi tempore dignissimos aspernatur et sunt ex?</div>
+    </div>
+</section>
+<section class="public_profile bgwhite superpost_top">
+    <div class="container">
+        <div class="col-xs-12 col-sm-6 col-md-4">
+        	<div class="info_profile">
+        		<img src="http://placehold.it/150x200" alt="" class="img-responsive">
+        		<h3>Non Profit Name</h3>
+        		<p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolvAenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec</p>
+        	</div>
+        </div>
+        <div class="col-xs-12 col-sm-6 col-md-7 col-md-offset-1">
+            <div class="tabs_info">
+
+                <!-- Nav tabs -->
+                <ul class="nav nav-tabs" role="tablist">
+                    <li role="presentation" class="active"><a href="#tab1" aria-controls="tab1" role="tab" data-toggle="tab">Tab Title</a></li>
+                    <li role="presentation"><a href="#tab2" aria-controls="tab2" role="tab" data-toggle="tab">Tab Title</a></li>
+                    <li role="presentation"><a href="#tab3" aria-controls="tab3" role="tab" data-toggle="tab">Tab Title</a></li>
+                </ul>
+
+                <!-- Tab panes -->
+                <div class="tab-content">
+                    <div role="tabpanel" class="tab-pane fade in active" id="tab1">...</div>
+                    <div role="tabpanel" class="tab-pane fade" id="tab2">...</div>
+                    <div role="tabpanel" class="tab-pane fade" id="tab3">...</div>
+                </div>
+
+            </div>
+        </div>
+    </div>
+</section>
