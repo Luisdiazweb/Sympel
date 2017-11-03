b846d5956021de33526914b32e4ee302abd97d2d
Victor Aguilar <valbert1993@gmail.com>
2017-11-03 16:21:08 -0600

    dont create users from dashboard

--- views/user/index.php
+++ views/user/index.php
@@ -18,7 +18,7 @@ $this->params['breadcrumbs'][] = $this->title;
     <?php // echo $this->render('_search', ['model' => $searchModel]); ?>
 
     <p>
-        <?= Html::a('Create Users System', ['create'], ['class' => 'btn btn-success']) ?>
+        <?php//= Html::a('Create Users System', ['create'], ['class' => 'btn btn-success']) ?>
     </p>
     <?php Pjax::begin(); ?>
     <?= GridView::widget([
