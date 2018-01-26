0b13cc88657fcf6553e436e8fc5fe472cf83f182
Luis Diaz <luisd@dccolorweb.com>
2018-01-26 15:36:14 -0600

    Add placeholder image
    Add a placeholder to the picture in the section of Posted by Section in
    Donation’s profile.

--- views/site/view_donation.php
+++ views/site/view_donation.php
@@ -91,7 +91,7 @@ use yii\widgets\DetailView;
     <div class="col-md-12 equal">
         <div class="col-md-3 col-sm-12 pl-0 donation-image-profile">
             <a href="#" class="donation-cta-image">
-                 <img class="float-xs-right rounded-circle img-border height-100 mx-auto d-block" src="/profiles_picture/58Lg3pkTl5IINq1cW_II2VxoAiqvEM_3.jpg" alt="Card image">
+                 <img class="float-xs-right rounded-circle img-border height-100 mx-auto d-block" src="/app-assets/images/carousel/05.jpg" alt="Card image">
             </a>
         </div>
         <div class="col-md-5 col-sm-12 pr-0">
