40293a7a5777837990eeae51ab75d90dd12bd68f
vmontoya <victormontoya@dccolorweb.com>
2018-03-28 22:19:36 -0600

    Fix issues with Sign up Process
    Fixed issues with the sign up process.
    Remove the "required" parameter from the image field in the registration
    process.

--- views/site/signup_step3_3.php
+++ views/site/signup_step3_3.php
@@ -119,7 +119,7 @@ $this->registerJsFile('@web/app-assets/js/scripts/forms/checkbox-radio.js',
                                                 <fieldset class="form-group">
                                                     <?= $form->field($profile, 'profile_picture_upload', [
                                                         'template' => '<label>{label}</label><label class="custom-file center-block block">{input}<span class="form-control-file"></span></label>'
-                                                    ])->fileInput(['multiple' => false, 'accept' => 'image/*', 'required' => true); ?>
+                                                    ])->fileInput(['multiple' => false, 'accept' => 'image/*', '' => true]); ?>
                                                 </fieldset>
                                             </div>
                                         </div>
